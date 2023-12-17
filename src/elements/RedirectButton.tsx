a95ff3903069d9049ffe259e961ad6399d77dac4
Pashs-ba <pashs.ba@gmail.com>
2023-12-17 18:34:51 +0300

    feat: fix for mobile

--- src/elements/RedirectButton.tsx
+++ src/elements/RedirectButton.tsx
@@ -1,17 +1,13 @@
+import ("../style/mainPage/redirectButton.css")
+
 export default function ({text, onClick}:{text: string, onClick: () => void}){
     return (
         <button type="button"
-                className="btn btn-primary rounded-4 mb-3 mt-2 fw-bold"
-                style={{
-                    padding: "1rem 1.5rem"
-                }}
+                className="btn btn-primary rounded-4 mb-3 mt-2 redirect-button"
                 onClick={onClick}
         >
             {text}
-            <i className="bi bi-arrow-down-right"
-               style={{
-                   marginLeft: "4rem",
-               }}
+            <i className="bi bi-arrow-down-right icon-button"
             ></i>
         </button>
     )
